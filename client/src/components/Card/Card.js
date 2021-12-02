e4549f91c1340179130df763295447d60146ba2c
DFalque <falque@me.com>
2021-12-02 22:27:39 +0000

    [client] change card phone element & style

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,7 +1,27 @@
 import "./Card.scss"
 
-const Cards = () => {
-	return <div></div>
+const Cards = (props) => {
+	const { name, description, manufacturer, price, ram, screen } = props
+	console.log(props)
+	return (
+		<div className="Card">
+			<div className="Card__image"></div>
+			<div className="Card__info">
+				<div>
+					<p className="Card__info--brand">{manufacturer}</p>
+				</div>
+				<p className="Card__info--name">{name}</p>
+				<p className="Card__info--phone"> {description}</p>
+				<div className="Card__info__price">
+					<p className="Card__info__price--button">Comprar</p>
+					<p className="Card__info__price--cost">
+						{price}
+						<span>€</span>
+					</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Cards
