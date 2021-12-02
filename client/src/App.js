04a657473eff69493d6d25d7b528e4f8ef2f27be
DFalque <falque@me.com>
2021-12-02 13:34:20 +0000

    [client] change app.js to call getAllPhones API

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
+// LIBRARIES
+import { useEffect, useState } from "react"
+//SERVICES
+import getAllPhones from "./services/getAllPhones"
+// OTHERS
 import "./App.scss"
 
 function App() {
+	const [catalogue, setCatalogue] = useState([])
+	useEffect(async () => {
+		const phones = await getAllPhones()
+		setCatalogue(phones)
+	}, [])
+
+	if (!catalogue) return null
+
 	return (
 		<div className="App">
 			<p className="paragraph">Phone Catalogue</p>
+			{catalogue.map((phone) => (
+				<p>{phone.name}</p>
+			))}
 		</div>
 	)
 }
