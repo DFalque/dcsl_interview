10c1f40dfc5cb698f7fdfb00d95b1269e4c23646
DFalque <falque@me.com>
2021-12-02 22:26:00 +0000

    [client] add getAllPhones data in app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react"
 // COMPONENTS
 import Nav from "./components/Nav"
+import Card from "./components/Card"
 //SERVICES
 import getAllPhones from "./services/getAllPhones"
 // OTHERS
@@ -9,19 +10,24 @@ import "./App.scss"
 
 function App() {
 	const [catalogue, setCatalogue] = useState([])
-	useEffect(async () => {
-		const phones = await getAllPhones()
-		setCatalogue(phones)
+
+	useEffect(() => {
+		async function getData() {
+			const phones = await getAllPhones()
+			setCatalogue(phones)
+		}
+		getData()
 	}, [])
 
 	if (!catalogue) return null
 	return (
 		<div className="App">
 			<Nav />
-			<p className="paragraph">Phone Catalogue</p>
-			{catalogue.map((phone) => (
-				<p>{phone.name}</p>
-			))}
+			<div className="container">
+				{catalogue.map((phone) => (
+					<Card key={phone.id} {...phone} />
+				))}
+			</div>
 		</div>
 	)
 }
