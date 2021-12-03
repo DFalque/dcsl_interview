0cd8ade68626d1c311ecd7462109a32f970ce144
DFalque <falque@me.com>
2021-12-03 15:12:33 +0000

    [client] add footer to BasicLayout

--- client/src/layout/BasicLayout/BasicLayout.js
+++ client/src/layout/BasicLayout/BasicLayout.js
@@ -1,11 +1,14 @@
 import "./BasicLayout.scss"
 import Nav from "../../components/Nav"
+import Footer from "../../components/Footer"
+
 function BasicLayout(props) {
 	const { children } = props
 	return (
 		<>
 			<Nav />
 			<main>{children}</main>
+			<Footer />
 		</>
 	)
 }
