653927b897fd3c22ea647abf712fe0b9a2e7e825
DFalque <falque@me.com>
2021-12-03 15:19:39 +0000

    [clinet] add navigation to Nav component

--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,15 +1,23 @@
+import { Link } from "react-router-dom"
 import "./Nav.scss"
 
 const Nav = () => {
 	return (
 		<nav className="Nav">
 			<div className="Nav__left">
-				<div className="Nav__left--logo" />
+				<Link to="/">
+					<div className="Nav__left--logo" />
+				</Link>
 			</div>
 			<div className="Nav__right">
 				<lu className="Nav__right__list">
 					<li>Colección</li>
-					<li>Catálogo</li>
+					<Link
+						to="/catalogue"
+						style={{ textDecoration: "none", color: "black" }}
+					>
+						<li>Catálogo</li>
+					</Link>
 					<li>Marcas</li>
 					<li>Idioma</li>
 				</lu>
