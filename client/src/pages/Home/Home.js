79ee23c9f388929c37c6ac568ef5faabae3674f2
DFalque <falque@me.com>
2021-12-03 15:20:05 +0000

    [client] add style Home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,23 @@
 import "./Home.scss"
-function Home() {
-	return <div>Esto es la Home</div>
+import mainLogo from "../../assets/img/Blogger.png"
+import Link from "react-router-dom/Link"
+
+function Home(props) {
+	return (
+		<div to="/catalogue" className="Home">
+			<section className="Home__shape left"></section>
+			<div className="Home__text">
+				<h1>Welcome to your favorite phone catalog</h1>
+				<Link to="/catalogue">
+					<Link to="/catalogue" className="Home__text__button">
+						See Catalogue
+					</Link>
+				</Link>
+			</div>
+			<img src={mainLogo} />
+			<section className="Home__shape right"></section>
+		</div>
+	)
 }
 
 export default Home
