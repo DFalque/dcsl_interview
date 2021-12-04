bee7c17d9140e86c083e4a4f61f16fd56d742d1a
DFalque <falque@me.com>
2021-12-04 14:04:37 +0000

    [client] add new services funcctions for use the API

---
+++ client/src/services/getPhone.js
@@ -0,0 +1,9 @@
+import axios from "axios"
+
+export default async function getPhone(id) {
+	const data = await axios.get(`http://localhost:3000/getphone/`, {
+		params: { id },
+	})
+
+	return data
+}
