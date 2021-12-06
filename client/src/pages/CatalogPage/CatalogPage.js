10964f005a817ac84344c1bbb2dcc3027a68fa72
DFalque <falque@me.com>
2021-12-06 01:06:27 +0000

    [client] change catalog page

---
+++ client/src/pages/CatalogPage/CatalogPage.js
@@ -0,0 +1,62 @@
+// LIBRARIES
+import { useEffect, useState } from "react"
+
+// COMPONENTS
+import Card from "../../components/Card"
+import ModalSimple from "../../components/ModalSimple"
+import AddPhoneForm from "../../components/Forms/AddPhoneForm/AddPhoneForm"
+import Header from "../../components/Header"
+import LoadingData from "../../components/LoadingData"
+
+//SERVICES
+import getAllPhones from "../../services/getAllPhones"
+// OTHERS
+import "./CatalogPage.scss"
+const CatalogPage = () => {
+	// STATES
+	const [catalog, setCatalog] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [showModal, setShowModal] = useState(false)
+	const [titleModal, setTittleModal] = useState("")
+	const [childrenModal, setChildrenModal] = useState(null)
+
+	//FUNCTIONS
+
+	async function getData() {
+		setLoading(true)
+		const phones = await getAllPhones()
+		setCatalog(phones)
+		setLoading(false)
+	}
+	const handlerModal = () => {
+		setTittleModal("Add new phone")
+		setChildrenModal(<AddPhoneForm />)
+		setShowModal(true)
+	}
+	// USE EFFECTS
+	useEffect(() => {
+		getData()
+	}, [])
+
+	return (
+		<>
+			<div className="CatalogPage">
+				<Header click={handlerModal} />
+				{loading ? (
+					<LoadingData />
+				) : (
+					<div className="CatalogPage__container">
+						{catalog.map((phone) => (
+							<Card key={phone._id} {...phone} />
+						))}
+					</div>
+				)}
+			</div>
+			<ModalSimple show={showModal} setShow={setShowModal} title={titleModal}>
+				{childrenModal}
+			</ModalSimple>
+		</>
+	)
+}
+
+export default CatalogPage
