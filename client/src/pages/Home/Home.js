d554b775c203055d20866a05dd0abdc65e11811b
DFalque <falque@me.com>
2021-12-06 01:07:00 +0000

    [client] change home page

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,22 +1,12 @@
+import Hero from "../../components/Hero"
+
 import "./Home.scss"
-import mainLogo from "../../assets/img/Blogger.png"
-import { Link } from "react-router-dom"
 
 function Home(props) {
 	return (
-		<div to="/catalogue" className="Home">
-			<section className="Home__shape left"></section>
-			<div className="Home__text">
-				<h1>Welcome to your favorite phone catalog</h1>
-				<div>
-					<Link to="/catalog" className="Home__text__button">
-						See Catalogue
-					</Link>
-				</div>
-			</div>
-			<img src={mainLogo} />
-			<section className="Home__shape right"></section>
-		</div>
+		<>
+			<Hero />
+		</>
 	)
 }
 
