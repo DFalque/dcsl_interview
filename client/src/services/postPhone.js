3ab3e0e9ac4cc15968fbf53366072e25b1f17b7c
DFalque <falque@me.com>
2021-12-06 01:04:35 +0000

    [client] change services postphone

--- client/src/services/postPhone.js
+++ client/src/services/postPhone.js
@@ -1,9 +1,7 @@
 import axios from "axios"
 
 export default async function postPhone(phone) {
-	const response = await axios.post(`http://localhost:3000/addphone`, {
-		phone,
-	})
+	const response = await axios.post(`http://localhost:3000/addphone`, phone)
 
 	return response
 }
