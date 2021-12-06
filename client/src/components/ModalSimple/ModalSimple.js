21d5ab1acf0a1a11f47117223d36c7ca57833f28
DFalque <falque@me.com>
2021-12-06 14:11:47 +0000

    [client] change Modal label

--- client/src/components/ModalSimple/ModalSimple.js
+++ client/src/components/ModalSimple/ModalSimple.js
@@ -11,9 +11,7 @@ const ModalSimple = (props) => {
 	return (
 		<Modal open={show} onClose={onClose} className="ModalSimple">
 			<div className="ModalSimple__container">
-				<h2 id="modal-modal-title" variant="h6" component="h2">
-					{title}
-				</h2>
+				<h2>{title}</h2>
 				{children}
 			</div>
 		</Modal>
