5e74af0424c0e9f34f4a7fb9636128eb0bf41036
DFalque <falque@me.com>
2021-12-09 00:54:08 +0000

    [client] add modal & button to Phone Page

--- client/src/pages/PhonePage/PhonePage.js
+++ client/src/pages/PhonePage/PhonePage.js
@@ -3,11 +3,24 @@ import { useParams } from "react-router-dom"
 import { useEffect, useState } from "react"
 import getPhone from "../../services/getPhone"
 import CircularProgress from "@mui/material/CircularProgress"
+import Header from "../../components/Header"
+import ModalSimple from "../../components/ModalSimple"
+import EditPhoneForm from "../../components/Forms/EditPhoneForm/EditPhoneForm"
+import ButtonBorder from "../../components/atoms/buttons/ButtonBorder/ButtonBorder"
 
 function PhonePage() {
 	const { id } = useParams()
 	const [dataPhone, setDataPhone] = useState({})
 	const [loading, setLoading] = useState(false)
+	const [showModal, setShowModal] = useState(false)
+	const [titleModal, setTittleModal] = useState("")
+	const [childrenModal, setChildrenModal] = useState(null)
+
+	const handlerModal = () => {
+		setTittleModal("Edit phone")
+		setChildrenModal(<EditPhoneForm />)
+		setShowModal(true)
+	}
 
 	async function getData() {
 		setLoading(true)
@@ -27,33 +40,37 @@ function PhonePage() {
 			</div>
 		)
 	return (
-		<div className="PhonePage">
-			<div
-				className="PhonePage__imageContainer"
-				style={{ backgroundImage: `url(${dataPhone.imageFileName})` }}
-			></div>
-			<div className="PhonePage__infoContainer">
-				<div className="PhonePage__infoContainer__header">
-					<h2 className="heading-secondary">{dataPhone.manufacturer}</h2>
-					<h1 className="heading-primary--sub">{dataPhone.name}</h1>
-				</div>
-				<p className="paragraph__main">{dataPhone.description}</p>
-				<ul className="PhonePage__infoContainer__list">
-					<li className="paragraph">{`RAM: ${dataPhone.ram}`}</li>
-					<li className="paragraph">{`CPU: ${dataPhone.processor}`}</li>
-					<li className="paragraph">{`Screen: ${dataPhone.screen}`}:</li>
-					<li className="paragraph">{`Color: ${dataPhone.color}`}</li>
-				</ul>
-				<div className="PhonePage__infoContainer__buy">
-					<button className="PhonePage__infoContainer__buy__button">
-						Añadir a la Cesta
-					</button>
-					<h1 className="heading-primary--main">
-						783<span className="paragraph">€</span>
-					</h1>
+		<>
+			<Header click={handlerModal} titleButton={"Edit"} title={"Details"} />
+			<div className="PhonePage">
+				<div
+					className="PhonePage__imageContainer"
+					style={{ backgroundImage: `url(${dataPhone.imageFileName})` }}
+				></div>
+				<div className="PhonePage__infoContainer">
+					<div className="PhonePage__infoContainer__header">
+						<h2 className="heading-secondary">{dataPhone.manufacturer}</h2>
+						<h1 className="heading-primary--sub">{dataPhone.name}</h1>
+					</div>
+					<p className="paragraph__main">{dataPhone.description}</p>
+					<ul className="PhonePage__infoContainer__list">
+						<li className="paragraph">{`RAM: ${dataPhone.ram}`}</li>
+						<li className="paragraph">{`CPU: ${dataPhone.processor}`}</li>
+						<li className="paragraph">{`Screen: ${dataPhone.screen}`}:</li>
+						<li className="paragraph">{`Color: ${dataPhone.color}`}</li>
+					</ul>
+					<div className="PhonePage__infoContainer__buy">
+						<ButtonBorder text="Add to buy" type="" size="big" />
+						<h1 className="heading-primary--main">
+							783<span className="paragraph">€</span>
+						</h1>
+					</div>
 				</div>
 			</div>
-		</div>
+			<ModalSimple show={showModal} setShow={setShowModal} title={titleModal}>
+				{childrenModal}
+			</ModalSimple>
+		</>
 	)
 }
 
